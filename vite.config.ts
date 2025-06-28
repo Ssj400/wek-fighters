4c526482b702f2b3c3c24ab0f8d70dcd785d88f9
ssj400 <gafetel2017@gmail.com>
2025-06-27 22:22:15 -0400

    Update configuration for deployement.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    allowedHosts: ["h6icl9-ip-186-189-85-59.tunnelmole.net"],
-  },
+  base: "/wek-fighters/",
 });
